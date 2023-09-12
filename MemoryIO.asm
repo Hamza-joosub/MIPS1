a0d12b7f60e2509eabb8fcb9bf06faf10345a424
Hamza <125697787+Hamza-joosub@users.noreply.github.com>
2023-09-12 21:06:25 +0200

    from mac

---
+++ MemoryIO.asm
@@ -0,0 +1,19 @@
+.text 
+.globl main
+
+main:
+
+la $t0, num1
+la $t1, num2
+la $t3, num3
+add $t3, $t1, $t2
+sw $t3, num3
+
+li $v0, 10
+syscall
+
+.data
+num1: .word 2
+num2: .word 2
+num3: .word 0
+
