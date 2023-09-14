c43e546ba7d7d2ffb9840c2a44285906152a55ce
Hamza-joosub <125697787+Hamza-joosub@users.noreply.github.com>
2023-09-14 13:25:04 +0200

    from pc

--- MemoryIO.asm
+++ MemoryIO.asm
@@ -4,16 +4,36 @@
 main:
 
 la $t0, num1
-la $t1, num2
-la $t3, num3
-add $t3, $t1, $t2
-sw $t3, num3
+lb $t1, ($t0)
+subi $t1, $t1, 48
+mul $t1, $t1, 10
+addi $t0, $t0, 1
+lb $t2, ($t0)
+subi $t2, $t2, 48
+add $t1, $t1, $t2
+
+la $t0, num2
+lb $t2, ($t0)
+subi $t2, $t2, 48
+mul $t2,$t2, 10
+addi $t0, $t0, 1
+lb $t3, ($t0)
+subi $t3, $t3, 48
+add $t2, $t2, $t3
+
+
+add $t1, $t1, $t2
+sw $t1, num3
+
+li $v0, 1
+lw $a0, num3
+syscall
 
 li $v0, 10
 syscall
 
 .data
-num1: .word 3
-num2: .word 3
+num1: .asciiz "32"
+num2: .asciiz "48"
 num3: .word 0
 
